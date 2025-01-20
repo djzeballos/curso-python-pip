dd61697126d8ee1411f684f7ea9ebd953bd1a028
dzeballos <djzeballos@gmail.com>
2025-01-19 22:35:06 -0400

    Pandas

--- app/main.py
+++ app/main.py
@@ -8,16 +8,27 @@ Clase 8/20  Curso de Python: PIP y Entornos virtuales
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+    '''
     data = read_csv.read_csv('data.csv')
     data = list(filter(lambda item: item['Continent'] == 'South America', data))
 
     countries = list(map(lambda x: x['Country/Territory'], data))
     percentages = list(map(lambda x: x['World Population Percentage'], data))
+    
     charts.generate_pie_chart(countries, percentages)
 
-    
+    '''
+    df = pd.read_csv('data.csv')                    # df = dataframe
+    df = df[df['Continent'] == 'Africa']
+
+    countries = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries, percentages)
+
+    data = read_csv.read_csv('data.csv')
     country = input('Type Country => ')
     print(country)
 
